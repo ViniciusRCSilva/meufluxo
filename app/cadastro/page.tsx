2d571b195cb79db2ec3bda52f47c6db8fb73c4d2
Vinicius <viniciusflexa@gmail.com>
2025-03-18 21:58:15 -0300

    style: make the pages be responsive for mobile

--- app/cadastro/page.tsx
+++ app/cadastro/page.tsx
@@ -11,21 +11,21 @@ const Register = async () => {
     }
 
     return (
-        <div className="grid grid-cols-[1.5fr_1fr] items-center min-h-screen">
-            <div className="flex flex-col md:flex-row items-center justify-center hover:scale-105 transition-transform duration-300">
+        <div className="grid md:grid-cols-[1.5fr_1fr] grid-cols-1 items-center min-h-screen">
+            <div className="flex flex-col items-center justify-center hover:scale-105 transition-transform duration-300 p-8 md:p-0">
                 <Image
                     src="/logo_meufluxo.svg"
                     alt="MeuFluxo Logo"
                     width={250}
                     height={250}
-                    className="drop-shadow-lg"
+                    className="drop-shadow-lg w-[150px] h-[150px] md:w-[250px] md:h-[250px]"
                 />
-                <div className="flex flex-col gap-4 text-center md:text-left select-none">
-                    <h1 className="text-5xl italic font-[family-name:var(--font-montserrat-alternates)]">MeuFluxo</h1>
-                    <p className="font-light font-[family-name:var(--font-poppins)]">Novo por aqui? Crie sua conta para começar a gerenciar seus finanças.</p>
+                <div className="flex flex-col gap-4 text-center select-none">
+                    <h1 className="text-4xl md:text-5xl italic font-[family-name:var(--font-montserrat-alternates)]">MeuFluxo</h1>
+                    <p className="font-light text-sm md:text-base font-[family-name:var(--font-poppins)]">Novo por aqui? Crie sua conta para começar a gerenciar seus finanças.</p>
                 </div>
             </div>
-            <div className="flex h-full w-full items-center justify-center bg-card-foreground">
+            <div className="flex h-full w-full items-center justify-center bg-card-foreground md:min-h-screen">
                 <RegisterForm />
             </div>
         </div>
--- app/login/page.tsx
+++ app/login/page.tsx
@@ -11,21 +11,21 @@ const Login = async () => {
     }
 
     return (
-        <div className="grid grid-cols-[1.5fr_1fr] items-center min-h-screen">
-            <div className="flex flex-col md:flex-row items-center justify-center hover:scale-105 transition-transform duration-300">
+        <div className="grid md:grid-cols-[1.5fr_1fr] grid-cols-1 items-center min-h-screen">
+            <div className="flex flex-col items-center justify-center hover:scale-105 transition-transform duration-300 p-8 md:p-0">
                 <Image
                     src="/logo_meufluxo.svg"
                     alt="MeuFluxo Logo"
                     width={250}
                     height={250}
-                    className="drop-shadow-lg"
+                    className="drop-shadow-lg w-[150px] h-[150px] md:w-[250px] md:h-[250px]"
                 />
-                <div className="flex flex-col gap-4 text-center md:text-left select-none">
-                    <h1 className="text-5xl italic font-[family-name:var(--font-montserrat-alternates)]">MeuFluxo</h1>
-                    <p className="font-light font-[family-name:var(--font-poppins)]">Simplificando o controle financeiro com clareza e eficiência.</p>
+                <div className="flex flex-col gap-4 text-center select-none">
+                    <h1 className="text-4xl md:text-5xl italic font-[family-name:var(--font-montserrat-alternates)]">MeuFluxo</h1>
+                    <p className="font-light text-sm md:text-base font-[family-name:var(--font-poppins)]">Simplificando o controle financeiro com clareza e eficiência.</p>
                 </div>
             </div>
-            <div className="flex h-full w-full items-center justify-center bg-card-foreground">
+            <div className="flex h-full w-full items-center justify-center bg-card-foreground md:min-h-screen">
                 <LoginForm />
             </div>
         </div>
