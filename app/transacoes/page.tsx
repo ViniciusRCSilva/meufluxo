f5729f60f3b850f5d5e89d939acb507a1a25be53
Vinicius <viniciusflexa@gmail.com>
2025-03-26 11:51:26 -0300

    feat: add filters for transactions table

--- app/transacoes/page.tsx
+++ app/transacoes/page.tsx
@@ -4,6 +4,8 @@ import { auth } from "@clerk/nextjs/server";
 import AddTransactionButton from "./_components/add-transaction-button";
 import { redirect } from "next/navigation";
 import { getTransactions } from "../_actions/transaction";
+import { HelpCircle } from "lucide-react";
+import { Tooltip, TooltipContent, TooltipTrigger } from "../_components/ui/tooltip";
 
 const Transacoes = async () => {
     const { userId } = await auth();
@@ -25,8 +27,18 @@ const Transacoes = async () => {
 
     return (
         <div className="flex flex-col gap-6 px-4 sm:px-10 pt-28 pb-10 font-[family-name:var(--font-poppins)]">
-            <div className="flex items-center justify-between">
-                <h1 className="text-xl font-semibold">Transações</h1>
+            <div className="flex flex-col lg:flex-row items-center gap-4 lg:justify-between">
+                <div className="flex items-center gap-2">
+                    <h1 className="text-xl font-semibold">Transações</h1>
+                    <Tooltip>
+                        <TooltipTrigger className="cursor-pointer">
+                            <HelpCircle className="h-6 w-6 text-font-muted" />
+                        </TooltipTrigger>
+                        <TooltipContent className="w-64 text-center">
+                            <p>Visualize, adicione e edite todas as transações que foram feitas na sua conta.</p>
+                        </TooltipContent>
+                    </Tooltip>
+                </div>
                 <AddTransactionButton userId={userId} />
             </div>
             <div>
