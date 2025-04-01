8c0227a14b55633ccba17e54c95d1474ee4a890c
Vinicius <viniciusflexa@gmail.com>
2025-04-01 20:30:49 -0300

    refactor: add to fixed to progress percentage

--- app/(home)/_components/financial-goal-progress-bar.tsx
+++ app/(home)/_components/financial-goal-progress-bar.tsx
@@ -10,7 +10,7 @@ const FinancialGoalProgressBar = ({ name, progress }: FinancialGoalProgressBarPr
         <div className="grid grid-cols-[1fr_2fr_1fr] lg:grid-cols-[1fr_4fr_1fr] gap-4 items-center">
             <p className="text-font w-20 lg:w-40 text-ellipsis overflow-hidden font-[family-name:var(--font-poppins)] text-left">{name}</p>
             <Progress value={progress} />
-            <p className="text-font-muted font-[family-name:var(--font-poppins)] text-right">{progress}%</p>
+            <p className="text-font-muted font-[family-name:var(--font-poppins)] text-right">{progress.toFixed(2)}%</p>
         </div>
     )
 }
