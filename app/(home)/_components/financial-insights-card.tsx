820d47a2bd7360240169ee76376dd7bc7f191b18
Vinicius <viniciusflexa@gmail.com>
2025-04-02 11:09:39 -0300

    feat: add carousel for financial insight card

--- app/(home)/_components/financial-insights-card.tsx
+++ app/(home)/_components/financial-insights-card.tsx
@@ -1,14 +1,29 @@
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/app/_components/ui/card"
+import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from "@/app/_components/ui/carousel"
 
 const FinancialInsightsCard = () => {
     return (
-        <Card>
+        <Card className="font-[family-name:var(--font-poppins)]">
             <CardHeader>
-                <CardTitle className="text-xl text-font-foreground font-[family-name:var(--font-poppins)]">Insights financeiros</CardTitle>
-                <CardDescription className="text-font-muted font-[family-name:var(--font-poppins)]">Análises e dicas para melhorar sua gestão financeira.</CardDescription>
+                <CardTitle className="text-xl text-font-foreground">Insights financeiros</CardTitle>
+                <CardDescription className="text-font-muted">Análises e dicas para melhorar sua gestão financeira.</CardDescription>
             </CardHeader>
-            <CardContent>
-                <p className="text-font font-[family-name:var(--font-poppins)]">Você precisa economizar R$ 750,00 este mês para pagar as contas pendentes.</p>
+            <CardContent className="px-16">
+                <Carousel opts={{ loop: true }} className="text-sm text-font">
+                    <CarouselContent>
+                        <CarouselItem>
+                            <p>Você precisa economizar R$ 750,00 este mês para pagar as contas pendentes.</p>
+                        </CarouselItem>
+                        <CarouselItem>
+                            <p>Você tem 2 metas definidas e 1 meta completa.</p>
+                        </CarouselItem>
+                        <CarouselItem>
+                            <p>Você gastou menos este mês em relação ao mês anterior.</p>
+                        </CarouselItem>
+                    </CarouselContent>
+                    <CarouselPrevious variant="outline" size="icon" className="text-font" />
+                    <CarouselNext variant="outline" size="icon" className="text-font" />
+                </Carousel>
             </CardContent>
         </Card>
     )
